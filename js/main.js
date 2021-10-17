523a863eea802b104fe2530dd93449f019dee023
xshah01 <shadaba@kth.se>
2021-10-17 14:24:37 +0200

    update

--- js/main.js
+++ js/main.js
@@ -28,3 +28,20 @@ window.addEventListener("scroll", () => {
         toTop.classList.remove("active");
     }
 })
+
+/* Reveal-on-scroll */
+window.addEventListener("scroll", reveal);
+
+function reveal() {
+
+    var reveals = document.querySelectorAll(".reveal");
+
+    for(var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var revealTop = reveals[i].getBoundingClientRect().top;
+        var revealPoint = 150;
+        if (revealTop < windowHeight - revealPoint) {
+            reveals[i].classList.add("active");
+        }
+    }
+}
